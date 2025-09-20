0d547ed9b8c3e5cde4b5aeddcd2f69945d7f540a
Angela Cabanes <c22-0927-423@uphsl.edu.ph>
2025-09-20 09:37:58 +0800

    Added changes to the code.

--- prelim-review/python/tokenExamples.py
+++ prelim-review/python/tokenExamples.py
@@ -1,1 +1,11 @@
+num = int(input("Enter an integer: ")) # 'num' is identifier
 
+if num % 2 == 0: 
+    # 'if' is a keyword
+    # '%' is an operator
+    # '2' is an integer literal
+    print(f"The number {num} is even. ⸜(｡˃ ᵕ ˂)⸝♡")
+    # 'print' is a built-in identifier.
+else:
+    # 'else' is a keyword.
+    print(f"The number {num} is odd. ⸜(｡˃ ᵕ ˂)⸝♡")
