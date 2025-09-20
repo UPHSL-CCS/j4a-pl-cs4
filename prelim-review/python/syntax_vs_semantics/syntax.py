bc3385ba65b12496695424d3b4c4149a08e7ee32
Angela Cabanes <c22-0927-423@uphsl.edu.ph>
2025-09-20 09:48:58 +0800

    Added explanation to the code.

--- prelim-review/python/syntax_vs_semantics/syntax.py
+++ prelim-review/python/syntax_vs_semantics/syntax.py
@@ -1,6 +1,5 @@
 def hellow(:
-    print("You are so stinky! ( ｡ •̀ ᴖ •́ ｡)")
+    print("You are so stinky! ( ｡ •̀ ᴖ •́ ｡))
+# syntax error since there are missing enclosing parenthesis and quotation mark
 
 hellow()
-
-# Angela Cabanes
