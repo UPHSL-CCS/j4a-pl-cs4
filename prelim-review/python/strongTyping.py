b2086b8bd2f951b09284df7099bafb453d663209
Angela Cabanes <c22-0927-423@uphsl.edu.ph>
2025-09-24 20:45:53 +0800

    Add python strongTyping.py

---
+++ prelim-review/python/strongTyping.py
@@ -0,0 +1,9 @@
+x = "5"     # string
+y = 10      # integer
+
+# can't add str + int directly
+# uncommenting the next line will cause an error
+# print(x + y)  
+
+# it would be correct if we used "print(int(x) + y)" instead, which will return as '15'.
+print(int(x) + y) 
