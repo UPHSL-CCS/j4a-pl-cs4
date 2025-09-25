ba458cb468e25bab41708d9518ff7c1b09909d91
Fraxinity <151002047+Fraxinity@users.noreply.github.com>
2025-09-25 23:41:36 +0800

    Add files via upload

---
+++ prelim-review/c++/Tokens.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
+
+// int = keyword
+// main = identifier
+// (), {}, ; = punctuation
+// cout = identifier
+// << = operator
+// "Hello, World" = String
