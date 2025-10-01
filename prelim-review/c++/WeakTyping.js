fd751225f392e3907e464fa0f6d71d01fad21b17
Fraxinity <151002047+Fraxinity@users.noreply.github.com>
2025-10-01 18:28:46 +0800

    Added WeakTyping.js, Tokens.cpp and GlobalVsLocal.cpp

---
+++ prelim-review/c++/WeakTyping.js
@@ -0,0 +1,7 @@
+let x = "5";  // string
+let y = 2;    // number
+
+// JS will automatically decide what to do
+
+console.log(x + y);  // "52"  (string concatenation)
+console.log(x * y);  // 10   (string converted to number)
