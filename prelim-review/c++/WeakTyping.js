0e228d5d5b597c3b2db2e734527c20ed4a569a87
Fraxinity <151002047+Fraxinity@users.noreply.github.com>
2025-10-01 18:27:47 +0800

    Delete prelim-review/c++/WeakTyping.js

--- prelim-review/c++/WeakTyping.js
+++
@@ -1,7 +0,0 @@
-let x = "5";  // string
-let y = 2;    // number
-
-// JS will automatically decide what to do
-
-console.log(x + y);  // "52"  (string concatenation)
-console.log(x * y);  // 10   (string converted to number)
