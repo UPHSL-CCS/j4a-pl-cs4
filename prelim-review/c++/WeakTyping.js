dca4052297fdf4dafdbedded24559c0cca8cbf4a
Fraxinity <151002047+Fraxinity@users.noreply.github.com>
2025-10-03 17:57:25 +0800

    Update WeakTyping.js

--- prelim-review/c++/WeakTyping.js
+++ prelim-review/c++/WeakTyping.js
@@ -3,5 +3,5 @@ let y = 2;    // number
 
 // JS will automatically decide what to do
 
-console.log(x + y);  // "52"  (string concatenation)
-console.log(x * y);  // 10   (string converted to number)
+console.log(x + y);  
+console.log(x * y); 
