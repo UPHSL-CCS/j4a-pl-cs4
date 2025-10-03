de347eef6cf181e77f06cb8dcee105ebc0baa72c
Fraxinity <151002047+Fraxinity@users.noreply.github.com>
2025-10-03 18:14:04 +0800

    Update Syntax Error.cpp

--- prelim-review/c++/syntax_vs_semantics/Syntax Error.cpp
+++ prelim-review/c++/syntax_vs_semantics/Syntax Error.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!"  // Missing semicolon here
+    cout << "Hello, World!"  // Missing semicolon here
     return 0;
+
 }
