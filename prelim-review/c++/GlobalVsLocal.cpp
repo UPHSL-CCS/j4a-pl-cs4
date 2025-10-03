455ff5d52c4088f88ab14e3d3a83a3f80d41d80d
Fraxinity <151002047+Fraxinity@users.noreply.github.com>
2025-10-03 17:55:44 +0800

    Update GlobalVsLocal.cpp

--- prelim-review/c++/GlobalVsLocal.cpp
+++ prelim-review/c++/GlobalVsLocal.cpp
@@ -5,13 +5,13 @@ int globalVar = 10;
  
 void printGlobal() {
 
-    cout << "Global variable value: " << globalVar << std::endl;
+    cout << "Global variable value: " << globalVar << endl;
     
 }
 int main() {
     int localVar = 20; 
-    cout << "Local variable value (inside main): " << localVar << std::endl;
-    cout << "Global variable value (inside main): " << globalVar << std::endl;
+    cout << "Local variable value (inside main): " << localVar << endl;
+    cout << "Global variable value (inside main): " << globalVar << endl;
     
     globalVar = 30;
     
