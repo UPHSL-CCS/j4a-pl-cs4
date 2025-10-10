2145c6e4c0f6157187195e45ff960d093819f687
Fraxinity <kelvinadam2004@gmail.com>
2025-10-10 15:41:58 +0800

    Making/testing calculator

---
+++ subprograms/Modular/Cookielator.h
@@ -0,0 +1,10 @@
+#ifndef Cookielator_h
+#define Cookielator_h
+
+// Function declarations for the 4 operators
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+#endif // Cookielator_h
