9d19c66403da8a18c8919a5994ddb8419530fdc0
Angela Cabanes <c22-0927-423@uphsl.edu.ph>
2025-10-10 17:11:41 +0800

    Add main.py demo script for only palindrome.

---
+++ subprograms/main.py
@@ -0,0 +1,24 @@
+from modules.palindrome import isPalindrome
+from modules.isogram import isIsogram
+from modules.tautonym import isTautonym
+
+
+def wordPlay():
+    while True:
+        text = input("\nEnter a word to check (or press Enter to quit): ").strip()
+
+        if text == "":
+            print("\nજ⁀➴Thank you for playing with words! ₍₍⚞(˶˃ ꒳ ˂˶)⚟⁾⁾♡\n")
+            break
+
+        print("")  # spacing
+
+        # Palindrome
+        if isPalindrome(text):
+            print(f"• '{text}' is a palindrome because it reads the same backward and forward.")
+        else:
+            print(f"• '{text}' is not a palindrome because it changes when reversed.")
+
+print("———————————————————————————♡———————————————————————————\n꒰  ⊹  ˚ . [ Word Play Subprogram ] !  ⁺  𓈒 ꒱\n-this program checks if the word you entered is either an isogram, palindrome, or tautonym.\nPress Enter without typing anything to exit.\n———————————————————————————♡———————————————————————————")
+
+wordPlay()
