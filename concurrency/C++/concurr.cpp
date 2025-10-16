ff69fc8f799208e569ca0e07297577155a191423
Fraxinity <kelvinadam2004@gmail.com>
2025-10-17 00:07:42 +0800

    Fixed overlapping problems

--- concurrency/C++/concurr.cpp
+++ concurrency/C++/concurr.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
+#include <mutex>
 using namespace std;
 
+mutex cookie_mutex;
+
 void task1() {
+
+    lock_guard<mutex> guard(cookie_mutex);
     for (int i = 0; i < 5; i++) {
         cout << "Tako 1 - Count: WAH " << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(50));
     }
 }
 
 void task2() {
+
+    lock_guard<mutex> guard(cookie_mutex);
     for (int i = 0; i < 5; i++) {
         cout << "Tako 2 - Count: WAH " << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(100));
     }
 }
 
