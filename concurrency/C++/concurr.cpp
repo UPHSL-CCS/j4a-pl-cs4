329107dfa13eb4ebdae0e46ccf2cdaf7aa988ef6
Fraxinity <kelvinadam2004@gmail.com>
2025-10-17 16:08:13 +0800

    Added comments on each part

--- concurrency/C++/concurr.cpp
+++ concurrency/C++/concurr.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 mutex cookie_mutex;
 
-void task1() {
+void task1() { // Prints out odd numbers
 
-    lock_guard<mutex> guard(cookie_mutex);
+    lock_guard<mutex> guard(cookie_mutex); // Lock the mutex for this scope
     for (int i = 0; i < 10; i++) {
         cout << "Tako 1 - Count: WAH " << i * 2 + 1 << endl;
     }
 }
 
-void task2() {
+void task2() { // Prints out even numbers
 
-    lock_guard<mutex> guard(cookie_mutex);
+    lock_guard<mutex> guard(cookie_mutex); // Lock the mutex for this scope
     for (int i = 0; i < 10; i++) {
         cout << "Tako 2 - Count: WAH " << i * 2 << endl;
 
@@ -25,11 +25,11 @@ void task2() {
 int main() {
     cout << "Initiating Takolization..." << endl;
 
-    thread t1(task1);
-    thread t2(task2);
+    thread t1(task1); // Start task1 in a new thread
+    thread t2(task2); // Start task2 in a new thread
 
-    t1.join();
-    t2.join();
+    t1.join(); // Wait for task1 to finish
+    t2.join(); // Wait for task2 to finish
 
     cout << "Both tako are complete." << endl;
     return 0;
