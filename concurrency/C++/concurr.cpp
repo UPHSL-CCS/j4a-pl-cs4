d7d974c4f891fd12b2103907205461fc1d942cce
Fraxinity <kelvinadam2004@gmail.com>
2025-10-17 13:55:49 +0800

    Changing the code to a different use

--- concurrency/C++/concurr.cpp
+++ concurrency/C++/concurr.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <thread>
-#include <chrono>
-#include <mutex>
+#include <mutex> // For std::mutex and std::lock_guard
 using namespace std;
 
 mutex cookie_mutex;
@@ -9,18 +8,17 @@ mutex cookie_mutex;
 void task1() {
 
     lock_guard<mutex> guard(cookie_mutex);
-    for (int i = 0; i < 5; i++) {
-        cout << "Tako 1 - Count: WAH " << i << endl;
-        this_thread::sleep_for(chrono::milliseconds(50));
+    for (int i = 0; i < 10; i++) {
+        cout << "Tako 1 - Count: WAH " << i * 2 << endl;
     }
 }
 
 void task2() {
 
     lock_guard<mutex> guard(cookie_mutex);
-    for (int i = 0; i < 5; i++) {
-        cout << "Tako 2 - Count: WAH " << i << endl;
-        this_thread::sleep_for(chrono::milliseconds(100));
+    for (int i = 0; i < 10; i++) {
+        cout << "Tako 2 - Count: WAH " << i * 2 + 1 << endl;
+
     }
 }
 
