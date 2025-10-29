a9e153cd24c60215da0d12c0e27305c3381d2efc
Fraxinity <kelvinadam2004@gmail.com>
2025-10-29 17:43:32 +0800

    Adding difficulty mode

--- pokemon-project/modules/game_logic.py
+++ pokemon-project/modules/game_logic.py
@@ -1,10 +1,11 @@
 class PokemonGame:
-    def __init__(self):
+    def init(self):
         self.lives = 3
-        self.current_pokemon= None
+        self.timer_duration = 60
+        self.current_pokemon = None
         self.caught_pokemon = []
-        self.got_away= []
-        self.game_active= False
+        self.got_away = []
+        self.game_active = False
 
     def start_game(self, difficulty):
         self.lives = 3
@@ -12,29 +13,34 @@ class PokemonGame:
         self.got_away = []
         self.game_active = True
 
-    
-    def load_new_pokemon_data(self,data):
-        self.current_pokemon = data #Loads new pokemon into the round
+        if difficulty == "Easy":
+            self.timer_duration = 120
+        elif difficulty == "Medium":
+            self.timer_duration = 60
+        else:
+            self.timer_duration = 30
 
-    def check_guess(self, guess):
+    def load_new_pokemon_data(self, data): #Loads new pokemon
+        self.current_pokemon = data
+
+    def check_guess(self, guess): # Checks the user's guess against the current Pokémon.
         if not self.current_pokemon:
             return None
-        
-        if guess.strip().lower()==self.current_pokemon["name"].lower():
+
+        if guess.strip().lower() == self.current_pokemon["name"].lower():
             self.caught_pokemon.append(self.current_pokemon["name"])
             return "correct"
         else:
             self.lives -= 1
-            self.got_away.append(self.current_pokemon["name"])
+            self.got_away.append(self.current_pokemon["name"]) # to show it in the final results.
             return "wrong"
-        
-    def handle_timeout(self):  #Handles when the player runs out of time.
+
+    def handle_timeout(self): # Handles the event when the timer runs out.
         if self.current_pokemon and self.current_pokemon["name"] not in self.got_away:
             self.got_away.append(self.current_pokemon["name"])
         self.lives -= 1
 
-
-    def get_score(self): #Final score calculation
+    def get_score(self): # Returns the current score and game statistics.
         return {
             "score": len(self.caught_pokemon),
             "caught": self.caught_pokemon,
