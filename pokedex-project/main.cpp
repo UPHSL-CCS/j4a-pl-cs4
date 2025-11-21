d524b24508ffe15e54d6b220734b147b7f6ddee3
Fraxinity <kelvinadam2004@gmail.com>
2025-11-21 13:41:42 +0800

    description fetch is fixed

--- pokedex-project/main.cpp
+++ pokedex-project/main.cpp
@@ -7,25 +7,9 @@ using namespace std;
 int main() {
     try {
 
-        auto future_pokemon = PokeAPI::fetchPokemon("Lucario");
+        auto future_pokemon = PokeAPI::fetchPokemonWithDescription("Lucario");
         Pokemon p = future_pokemon.get();
-
-        cout << "ID: " << p.id << endl;
-        cout << "Name: " << p.name << endl;
-        cout << "Height: " << p.height << endl;
-        cout << "Weight: " << p.weight << endl;
-        cout << "Types: ";
-        for (const auto& type : p.types) {
-            cout << type << " ";
-        }
-        cout << endl;
-        cout << "Stats: " << endl;
-        for (const auto& stat : p.stats) {
-            cout << "  " << stat.name << ": " << stat.base_stat << endl;
-        }
-
-        cout << "Sprite URL: " << p.sprite_url << endl;
-        
+        cout << "Fetched: " << p.name << std::endl;
         cout << "Description: " << p.description << endl;
 
     } catch (const exception& e) {
